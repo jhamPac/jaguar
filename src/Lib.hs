9a18b3a85b9033b5461caa85ba9c47108cbe9bc7
jhamPac <cloud@kevia.me>
2021-08-24 08:28:22 -0700

    chore: minor spacing

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( run
-    ) where
+  ( run,
+  )
+where
 
+import Data.Monoid (mconcat)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import Web.Scotty
-import Data.Monoid (mconcat)
 
 run :: IO ()
 run = scotty 9000 $
-    get "/" $
-        html $ renderHtml $
-            H.html $
-                H.body $ do
-                    H.h1 "Jaguar, the vicious web server!"
+  get "/" $
+    html $
+      renderHtml $
+        H.html $
+          H.body $ do
+            H.h1 "Jaguar, the vicious web server!"
