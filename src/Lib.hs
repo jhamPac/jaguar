5ca9aec344e66d7d8b4503c9df089dd927e1e7ce
jhamPac <cloud@kevia.me>
2021-08-23 20:25:03 -0700

    feat: use blaze-html

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,15 @@ module Lib
     ( run
     ) where
 
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import qualified Text.Blaze.Html5 as H
 import Web.Scotty
 import Data.Monoid (mconcat)
 
 run :: IO ()
 run = scotty 9000 $
-    get "/" $ html "<h1>Jaguar</h1>"
+    get "/" $
+        html $ renderHtml $
+            H.html $
+                H.body $ do
+                    H.h1 "Jaguar, the vicious web server!"
