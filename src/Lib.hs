25416718d70c7b11141fe5baa024164d15c53f38
jhamPac <cloud@kevia.me>
2021-09-06 20:07:29 -0700

    feat: create input forms functionally

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,9 +5,10 @@ module Lib
   )
 where
 
-import           Data.Monoid                   (mconcat)
-import           Text.Blaze.Html.Renderer.Text (renderHtml)
-import qualified Text.Blaze.Html5              as H
+import           Data.Monoid                        (mconcat)
+import           Text.Blaze.Html.Renderer.Text      (renderHtml)
+import qualified Text.Blaze.Html4.Strict.Attributes as A
+import qualified Text.Blaze.Html5                   as H
 import           Web.Scotty
 
 run :: IO ()
@@ -17,4 +18,8 @@ run = scotty 9000 $
             renderHtml $
                 H.html $
                     H.body $ do
-                        H.h1 "Jaguar, the vicious web server!"
+                        H.h1 "Shortener"
+                        H.form H.! A.method "post" H.! A.action "/" $ do
+                            H.input H.! A.type_ "text" H.! A.name "url"
+                            H.input H.! A.type_ "submit"
+
