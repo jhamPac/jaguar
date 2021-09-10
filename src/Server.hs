282b30352d099a63bbbba41a4477a24730f653ee
jhamPac <cloud@kevia.me>
2021-09-10 13:53:37 -0700

    refactor: comnent out code for possible features for now

--- src/Server.hs
+++ src/Server.hs
@@ -9,11 +9,7 @@ import           Control.Monad.IO.Class             (MonadIO (liftIO))
 import           Data.ByteString.Char8
 import           Data.Foldable                      (for_)
 import           Data.Hashable
-import           Data.IORef                         (modifyIORef, newIORef,
-                                                     readIORef)
-import           Data.Map                           (Map)
 import qualified Data.Map                           as M
-import           Data.Monoid                        (mconcat)
 import           Data.Text                          (Text)
 import           Data.Text.Encoding                 (encodeUtf8)
 import qualified Data.Text.Lazy                     as LT
@@ -35,10 +31,8 @@ sanitizeKey = pack . show . abs . hash' . encodeUtf8
 run :: IO ()
 run = do
     conn <- R.checkedConnect R.defaultConnectInfo
-    urlsR <- newIORef (1 :: Int, mempty :: Map Int Text)
     scotty 9000 $ do
         get "/" $ do
-            (_, urls) <- liftIO $ readIORef urlsR
             html $
                 renderHtml $
                     H.html $
@@ -47,22 +41,19 @@ run = do
                             H.form H.! A.method "post" H.! A.action "/" $ do
                                 H.input H.! A.type_ "text" H.! A.name "url"
                                 H.input H.! A.type_ "submit"
-                            H.table $
-                                for_ (M.toList urls) $ \(i, url) ->
-                                    H.tr $ do
-                                        H.td (H.toHtml i)
-                                        H.td (H.text url)
+                            -- H.table $
+                            --     for_ (M.toList urls) $ \(i, url) ->
+                            --         H.tr $ do
+                            --             H.td (H.toHtml i)
+                            --             H.td (H.text url)
         post "/" $ do
             url <- param "url"
-            liftIO $ modifyIORef urlsR $
-                \(i, urls) ->
-                    (i + 1, M.insert i url urls)
             liftIO $ R.runRedis conn $ do R.set (sanitizeKey url) (encodeUtf8 url)
             redirect "/"
-        get "/:n" $ do
-            n <- param "n"
-            (_, urls) <- liftIO $ readIORef urlsR
-            case M.lookup n urls of
-                Just url -> redirect (LT.fromStrict url)
-                Nothing  -> raiseStatus status404 "not found"
+        -- get "/:n" $ do
+        --     n <- param "n"
+        --     (_, urls) <- liftIO $ readIORef urlsR
+        --     case M.lookup n urls of
+        --         Just url -> redirect (LT.fromStrict url)
+        --         Nothing  -> raiseStatus status404 "not found"
 
